a405a572a5f995f4a7a8572d35a93af06f2014f5
David Souther <davidsouther+github@gmail.com>
2022-04-10 09:01:02 -0700

    Remove random and UUID, as they are available in Crypto

--- jiffies/random/browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} size
- * @returns Uint8Array
- */
-export function getUint8Array(size) {
-	const array = new Uint8Array(size);
-	window.crypto.getRandomValues(array);
-	return array;
-}
--- jiffies/test_all.js
+++ jiffies/test_all.js
@@ -3,26 +3,16 @@ import { describe, expect, it } from "./scope/index.js";
 import * as result from "./result.test.js";
 import * as equal from "./equal.test.js";
 import * as lock from "./lock.test.js";
-import * as uuid from "./uuid.test.js";
 import * as context from "./context.test.js";
 import * as flags from "./flags.test.js";
 import * as generator from "./generator.test.js";
 
-describe(
-	"Test executor",
-	() => {
-		it(
-			"matches equality",
-			() => {
-				expect(1).toBe(1);
-			},
-		);
+describe("Test executor", () => {
+  it("matches equality", () => {
+    expect(1).toBe(1);
+  });
 
-		it(
-			"fails on inequality",
-			() => {
-				expect(() => expect(1).toBe(2)).toThrow();
-			},
-		);
-	},
-);
+  it("fails on inequality", () => {
+    expect(() => expect(1).toBe(2)).toThrow();
+  });
+});
