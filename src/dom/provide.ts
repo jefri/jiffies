aa03254c321502a274325f24360f3bd14ecf67ac
David Souther <davidsouther+github@gmail.com>
2022-05-02 08:47:36 -0700

    Add initial DI registry

---
+++ src/dom/provide.ts
@@ -0,0 +1,11 @@
+import { Option } from "../result.js";
+
+let registry: Record<string, unknown> = {};
+
+export function provide(items: Record<string, unknown>) {
+  registry = { ...registry, ...items };
+}
+
+export function retrieve<T>(key: string): Option<T> {
+  return registry[key] as T;
+}
