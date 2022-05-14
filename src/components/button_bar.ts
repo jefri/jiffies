566543238e20dafec2dc2d05b91f79cbbcce165d
David Souther <davidsouther+github@gmail.com>
2022-05-13 18:34:56 -0700

    Add ButtonGroup style and use it for ButtonBar

--- src/components/button_bar.ts
+++ src/components/button_bar.ts
@@ -1,6 +1,9 @@
-import { Display, display } from "../display.js";
+import { display } from "../display.js";
 import { FC } from "../dom/fc.js";
-import { a, li, ul } from "../dom/html.js";
+import { div, fieldset, input, label } from "../dom/html.js";
+
+let buttonBarId = 1;
+let nextId = () => buttonBarId++;
 
 const ButtonBar = FC<{
   // T extends Display
@@ -10,29 +13,29 @@ const ButtonBar = FC<{
   values: T[];
   // @ts-ignore TODO(TFC)
   events: { onSelect: (current: T) => void };
-}>("button-bar", (el, { value, values, events }) =>
-  ul(
-    { class: "ButtonBar__wrapper" },
-    ...values.map((option) =>
-      li(
-        a(
-          {
-            href: "#",
-            class: `ButtonBar__${`${option}`.replace(/\s+/g, "_").toLowerCase()}
-                ${option === value ? "" : "secondary"}
-                `.replace(/[\n\s]+/, " "),
+}>("button-bar", (el, { value, values, events }) => {
+  const name = `button-bar-${nextId()}`;
+  return fieldset(
+    { class: "input-group" },
+    ...values
+      .map((option) => {
+        const opt = `${option}`.replace(/\s+/g, "_").toLowerCase();
+        const id = `${name}-${opt}`;
+        return [
+          input({
+            type: "radio",
+            id,
+            name,
+            value: option,
+            checked: option === value,
             events: {
-              click: (e) => {
-                e.preventDefault();
-                events.onSelect(option);
-              },
+              change: () => events.onSelect(option),
             },
-          },
-          display(option)
-        )
-      )
-    )
-  )
-);
-
+          }),
+          label({ role: "button", htmlFor: id }, display(option)),
+        ];
+      })
+      .flat()
+  );
+});
 export default ButtonBar;
