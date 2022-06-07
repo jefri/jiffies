09f57d443e695826bb563a68a14ee42eab3c83ad
David Souther <davidsouther+github@gmail.com>
2022-06-07 15:42:43 -0700

    Add diff tooling to find errors in similar object trees

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -1,5 +1,43 @@
+import { diff } from "./diff.js";
 import { describe, it } from "./scope/describe.js";
+import { expect } from "./scope/expect.js";
 
 describe("diff", () => {
-  it("diffs primitives", () => {});
+  it("diffs primitives", () => {
+    const diffed = diff({ a: 1, b: 2 }, { a: 2, b: 2 });
+    expect(diffed).toEqual([{ key: "a", left: 1, right: 2 }]);
+  });
+
+  it("diffs objects", () => {
+    const diffed = diff({ a: { c: 1 }, b: 2 }, { a: { c: 2 }, b: 2 });
+    expect(diffed).toEqual([
+      { key: "a", children: [{ key: "c", left: 1, right: 2 }] },
+    ]);
+  });
+
+  it("diffs missing sides", () => {
+    const diffed = diff<{ a?: number; b?: number }>({ a: 1 }, { b: 2 });
+    expect(diffed).toEqual([
+      { key: "a", left: 1, right: undefined },
+      { key: "b", left: undefined, right: 2 },
+    ]);
+  });
+
+  it("diffs arrays", () => {
+    const diffed = diff<number[]>([1, 2, 3], [1, 4, 3]);
+    expect(diffed).toEqual([{ key: 1, left: 2, right: 4 }]);
+  });
+
+  it("diffs objects in an array", () => {
+    const diffed = diff(
+      [{ a: { b: 1 } }, { a: { b: 2 } }, { a: { b: 3 } }],
+      [{ a: { b: 1 } }, { a: { b: 4 } }, { a: { b: 3 } }]
+    );
+    expect(diffed).toEqual([
+      {
+        key: 1,
+        children: [{ key: "a", children: [{ key: "b", left: 2, right: 4 }] }],
+      },
+    ]);
+  });
 });
--- src/diff.ts
+++ src/diff.ts
@@ -1,35 +1,80 @@
-import { None, Ok, Option, Some } from "./result.js";
+import { range } from "./range.js";
+import { isSome, None, Option, Some } from "./result.js";
 
 export const DiffA = Symbol("A");
 export const DiffB = Symbol("B");
 
-interface Diff {
-  [DiffA]: string | number | boolean;
-  [DiffB]: string | number | boolean;
-  [k: string | number]: Diff;
+export type DiffIndex = string | number;
+export type DiffPrimitive = string | number | boolean | null | undefined;
+
+interface DiffEntry {
+  key: DiffIndex;
+  left: DiffPrimitive;
+  right: DiffPrimitive;
 }
 
-export function diff<T>(a: Partial<T>, b: Partial<T>): Option<Diff> {
-  return Some({
-    [DiffA]: JSON.stringify(a),
-    [DiffB]: JSON.stringify(b),
-  });
-  // // runtime type checking
-  // switch (typeof a) {
-  //   case "boolean" | "number" | "string":
-  //     if (!Object.is(a, b)) {
-  //       return Ok({ [DiffA]: a, [DiffB]: b });
-  //     }
-  //   case "object":
-  //     if (b === undefined) {
-  //       return Ok({ [DiffA]: "[object Object]", [DiffB]: "undefined" });
-  //     }
-  //     if (a instanceof Array && b instanceof Array) {
-  //       return diffArrays(a, b);
-  //     } else {
-  //       return diffObjects(a, b);
-  //     }
-  //   default:
-  // }
-  // return None();
+interface DiffList {
+  key: DiffIndex;
+  children: (DiffEntry | DiffList)[];
+}
+
+function doDiff<T>(va: T, vb: T, k: DiffIndex): Option<DiffList | DiffEntry> {
+  if (Array.isArray(va)) {
+    // @ts-ignore
+    return diffArray(va, vb, k);
+  }
+  if (typeof va == "object") {
+    const d = diffObject(va, vb, k);
+    if (d.children.length == 0) {
+      return None();
+    } else {
+      return Some(d);
+    }
+  }
+  if (Object.is(va, vb)) {
+    return None();
+  } else {
+    // @ts-ignore
+    return { key: k, left: va, right: vb };
+  }
+}
+
+function diffArray<T>(
+  a: Partial<T>[],
+  b: Partial<T>[],
+  key: DiffIndex
+): Option<DiffList> {
+  const indexes = Math.max(a.length, b.length);
+  const children = range(0, indexes)
+    .map((i) => doDiff(a[i], b[i], i))
+    .filter(isSome);
+  return children.length > 0 ? { key, children } : None();
+}
+
+function diffObject<T>(
+  a: Partial<T>,
+  b: Partial<T>,
+  key: DiffIndex = ""
+): DiffList {
+  const keys = new Set([...Object.keys(a), ...Object.keys(b)]);
+  const children = [...keys]
+    // @ts-ignore
+    .map((k) => doDiff(a[k], b[k], k))
+    .filter(isSome);
+  return {
+    key,
+    children,
+  };
+}
+
+export function diff<T>(
+  a: Partial<T>,
+  b: Partial<T>
+): (DiffEntry | DiffList)[] {
+  return (
+    Array.isArray(a)
+      ? // @ts-ignore
+        diffArray(a, b, "") ?? { children: [] }
+      : diffObject(a, b, "")
+  ).children;
 }
