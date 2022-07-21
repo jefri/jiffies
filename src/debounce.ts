bea26e9cec6ed24bf7644dfa82f6e46537653a8e
David Souther <davidsouther+github@gmail.com>
2022-07-20 20:01:21 -0700

    Return timer from debounce

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,7 +1,8 @@
 export function debounce(fn: (...args: any[]) => any, ms = 32) {
   let timer: ReturnType<typeof setTimeout>;
-  return (...args: Parameters<typeof fn>): ReturnType<typeof fn> => {
+  return (...args: Parameters<typeof fn>) => {
     clearTimeout(timer);
     timer = setTimeout(() => (clearTimeout(timer), fn(...args)), ms);
+    return timer;
   };
 }
